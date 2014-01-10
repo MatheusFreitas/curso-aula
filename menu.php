356c7e9f8d9bc0d9375a4b5bc4ca2c7bc17807fe
MatheusFreitas <matheus.freitas47@yahoo.com.br>
2014-01-10 16:16:46 -0200

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,7 @@
+<a href="index.php">listar</a>
+&nbsp;&nbsp;
+<a href="form_cadastrar.php">Cadastrar</a>
+&nbsp;&nbsp;
+<a href="form_remover.php">Remover</a>
+
+<a href="form_editar.php">Editar</a>
