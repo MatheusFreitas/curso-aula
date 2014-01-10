f51dba957644ef05f2e035c0c95c0b147714169f
MatheusFreitas <matheus.freitas47@yahoo.com.br>
2014-01-10 16:12:35 -0200

    Create campos_pessoa.php

---
+++ campos_pessoa.php
@@ -0,0 +1,23 @@
+<label>Nome: </label>
+<input type="text" name="nome" />
+<br/>
+<input type="radio" name="sexo" value="masculino" checked="checked" />
+<label>Masculino</label>
+<input type="radio" name="sexo" value="feminino" />
+<label>Feminino</label>
+<br/>
+<input type="checkbox" name="aceito" value="sim" />
+<label>Aceito o contrato</label>
+<br/>
+<label>Estado</label>
+<select name="estado">
+    <option>São Paulo</option>
+    <option>Rio De Janeiro</option>
+    <option>Bahia</option>
+    <option>Manaus</option>
+</select>
+<br/>
+<label>Observaçãoes</label>
+<br/>
+<textarea name="observacoes"></textarea>
+<br/>
